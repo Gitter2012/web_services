0cdec7c45e025be3aa92f3199de10c391e928345
Gitter2012 <dragon.200809@163.com>
2026-02-19 16:58:51 +0800

    ADD: fix crawler

--- ResearchPulse/apps/crawler/factory.py
+++ ResearchPulse/apps/crawler/factory.py
@@ -142,7 +142,7 @@ class CrawlerFactory:
             config['category'] = source.code
         if hasattr(source, 'feed_url'):
             config['feed_url'] = source.feed_url
-        if hasattr(source, 'id'):
+        if hasattr(source, 'id') and not hasattr(source, 'code'):
             config['feed_id'] = str(source.id)
         if hasattr(source, 'feed_type'):
             config['feed_type'] = source.feed_type
